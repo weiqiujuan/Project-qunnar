372a97c06e5e797cf1e1e3ec6718018663e3cfce
CallmePapa <1062927638@qq.com>
2018-11-09 19:59:55 +0800

    node-server

---
+++ node-server/data/data.js
@@ -0,0 +1,76 @@
+let data = [
+    {
+        startTime: '06:36',
+        endTime: '08:50',
+        startAddress: '首都T3',
+        endAddress: '虹桥T2',
+        price: '￥484',
+        dec: '经济舱3.9折',
+        state: '持续涨价中'
+    },
+    {
+        startTime: '07:35',
+        endTime: '09:10',
+        startAddress: '首都T3',
+        endAddress: '虹桥T2',
+        price: '￥930',
+        dec: '经济舱7.4折',
+        state: '持续涨价中'
+    },
+    {
+        startTime: '08:10',
+        endTime: '10:50',
+        startAddress: '首都T3',
+        endAddress: '虹桥T2',
+        price: '￥1501',
+        dec: '头等舱6.3折',
+        state: ''
+    },
+    {
+        startTime: '06:36',
+        endTime: '08:50',
+        startAddress: '首都T3',
+        endAddress: '虹桥T2',
+        price: '￥484',
+        dec: '经济舱3.9折',
+        state: '持续涨价中'
+    },
+    {
+        startTime: '07:35',
+        endTime: '09:10',
+        startAddress: '首都T3',
+        endAddress: '虹桥T2',
+        price: '￥930',
+        dec: '经济舱7.4折',
+        state: '持续涨价中'
+    },
+    {
+        startTime: '08:10',
+        endTime: '10:50',
+        startAddress: '首都T3',
+        endAddress: '虹桥T2',
+        price: '￥1501',
+        dec: '头等舱6.3折',
+        state: ''
+    },
+    {
+        startTime: '07:35',
+        endTime: '09:10',
+        startAddress: '首都T3',
+        endAddress: '虹桥T2',
+        price: '￥930',
+        dec: '经济舱7.4折',
+        state: '持续涨价中'
+    },
+    {
+        startTime: '08:10',
+        endTime: '10:50',
+        startAddress: '首都T3',
+        endAddress: '虹桥T2',
+        price: '￥1501',
+        dec: '头等舱6.3折',
+        state: ''
+    }
+]
+
+module.exports = data
